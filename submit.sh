105575e2321a7ec1f22d9259b329a5f9ad4fc798
JadnjungSpector <jungjj@cs.washington.edu>
2025-01-26 18:50:19 -0800

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -6,7 +6,7 @@ rm -rf submit submit.zip
 mkdir -p submit
 
 # submit team.txt
-printf "Victor Zhong,vzhong\nNoah Smith,nasmith\nSanta Claus,sclaus" > submit/team.txt
+printf "Jaden Jung,jungjj\nEric Nguyen,nguyenr\nJacobYoung,jyoung8" > submit/team.txt
 
 # train model
 python src/myprogram.py train --work_dir work
